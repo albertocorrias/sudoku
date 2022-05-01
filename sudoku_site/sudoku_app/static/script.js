3c742e612620786c07d5a25d63be0b25f7ed360f
Alberto Corrias <alberto@nus.edu.sg>
2022-05-01 23:51:18 +0800

    skeleton numpad on the right

--- sudoku_site/sudoku_app/static/script.js
+++ sudoku_site/sudoku_app/static/script.js
@@ -3,5 +3,4 @@ import Game from "./Game.js"
 
 const puzzle_hints = JSON.parse(document.getElementById('puzzle_hints').textContent);
 const puzzle_solution = JSON.parse(document.getElementById('puzzle_solution').textContent);
-console.log(puzzle_solution)
 const new_game = new Game(puzzle_hints,puzzle_solution);
