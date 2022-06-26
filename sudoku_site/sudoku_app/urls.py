82c4a268b994827100b06bd8b300aae06e1252b3
Alberto Corrias <alberto@nus.edu.sg>
2022-06-26 22:57:05 +0800

    minor url change

--- sudoku_site/sudoku_app/urls.py
+++ sudoku_site/sudoku_app/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'sudoku_app'
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('/', views.index, name='index'),
     path('/new_puzzle', views.new_puzzle, name='new_puzzle'),
     path('/<int:puzzle_id>', views.new_specific_puzzle, name='new_spec_puzzle'),
