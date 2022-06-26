e2095f1d668c10ce92bccccaa65e505699b3cb55
Alberto Corrias <alberto@nus.edu.sg>
2022-06-26 10:17:42 +0800

    making the current difficulty level model useless

--- sudoku_site/sudoku_app/views.py
+++ sudoku_site/sudoku_app/views.py
@@ -8,28 +8,43 @@ import random
 
 from sudoku_app.forms import DifficultyLevelForm
 def index(request):
-    template = loader.get_template('sudoku_app/index.html')
+    
     if (Game.objects.all().count()==0):
         GenerateDatabase();
     #Check if any current difficulty level was set
-    diff_level = Game.EASY
-    if (CurrentDifficultyLevel.objects.all().count() == 0):
-        CurrentDifficultyLevel.objects.create(current_level=Game.EASY)
-    else:
-        diff_level = CurrentDifficultyLevel.objects.values_list("current_level", flat=True)[0]
+    # diff_level = Game.EASY
+    # if (CurrentDifficultyLevel.objects.all().count() == 0):
+    #     CurrentDifficultyLevel.objects.create(current_level=Game.EASY)
+    # else:
+    #     diff_level = CurrentDifficultyLevel.objects.values_list("current_level", flat=True)[0]
     
 
-    game_objects = Game.objects.filter(difficulty = diff_level)
+    # game_objects = Game.objects.filter(difficulty = diff_level)
+    # sel_idx = random.randint(0,game_objects.count()-1)
+    # puzzle = game_objects[sel_idx]
+    
+    # diff_level_form = DifficultyLevelForm(initial = {'difficulty_level' : diff_level})
+    # context = {
+    #     'puzzle_hints': puzzle.hints_board,
+    #     'puzzle_solution': puzzle.solved_board,
+    #     'diff_level_form' : diff_level_form
+    # }
+    context = set_up_new_puzzle(Game.EASY)
+    template = loader.get_template('sudoku_app/index.html')
+    return HttpResponse(template.render(context, request))
+
+def set_up_new_puzzle(diffiulty_level):
+    game_objects = Game.objects.filter(difficulty = diffiulty_level)
     sel_idx = random.randint(0,game_objects.count()-1)
     puzzle = game_objects[sel_idx]
     
-    diff_level_form = DifficultyLevelForm(initial = {'difficulty_level' : diff_level})
-    context = {
+    diff_level_form = DifficultyLevelForm(initial = {'difficulty_level' : diffiulty_level})
+    ret = {
         'puzzle_hints': puzzle.hints_board,
         'puzzle_solution': puzzle.solved_board,
         'diff_level_form' : diff_level_form
     }
-    return HttpResponse(template.render(context, request))
+    return ret
 
 def new_puzzle(request):
     if request.method =='POST':
@@ -40,4 +55,6 @@ def new_puzzle(request):
             first_id = CurrentDifficultyLevel.objects.first().id
             CurrentDifficultyLevel.objects.filter(id = first_id).update(current_level = supplied_diff_level)
 
-    return HttpResponseRedirect(reverse('sudoku_app:index'));
+            context = set_up_new_puzzle(supplied_diff_level)
+    template = loader.get_template('sudoku_app/index.html')
+    return HttpResponse(template.render(context, request));
