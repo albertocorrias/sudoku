43e647ac57a69f9cd4129df9914d34f43158f60f
Alberto Corrias <alberto@nus.edu.sg>
2022-06-26 17:45:41 +0800

    handling error of non-existing puzzle

--- sudoku_site/sudoku_app/views.py
+++ sudoku_site/sudoku_app/views.py
@@ -24,22 +24,43 @@ def set_up_new_puzzle(puzzle_id):
     A helper method that, given a puzzle id, returns a dictionary with
     - A game board object with hints. This is chosen randomly from the database
     - A game board object with he solution.This is chosen randomly from the database
-    - A form for further changes to the difficulty level. the form is initialized to the given difficulty level
+    - A form for further changes to the difficulty level. The form is initialized to the given difficulty level
+    - 
     '''
-    puzzle = Game.objects.filter(id=puzzle_id).get()
-    difficulty_level = puzzle.difficulty
-    diff_level_form = DifficultyLevelForm(initial = {'difficulty_level' : difficulty_level})
-    ret = {
-        'puzzle_hints': puzzle.hints_board,
-        'puzzle_solution': puzzle.solved_board,
-        'diff_level_form' : diff_level_form
-    }
+    error_code = 0;
+    puzzle_qs = Game.objects.filter(id=puzzle_id);
+    if (puzzle_qs.count() == 0):error_code = 1
+    if (puzzle_qs.count() >1): error_code = 1
+
+    if (error_code ==0):
+        puzzle = puzzle_qs.get() #the actual object
+        difficulty_level = puzzle.difficulty
+        diff_level_form = DifficultyLevelForm(initial = {'difficulty_level' : difficulty_level})
+        ret = {
+            'puzzle_hints': puzzle.hints_board,
+            'puzzle_solution': puzzle.solved_board,
+            'diff_level_form' : diff_level_form,
+            'error_code' : error_code,
+            'puzzle_id' : puzzle_id
+        }
+    else:
+        ret = {
+            'puzzle_hints': None,
+            'puzzle_solution': None,
+            'diff_level_form' : None,
+            'error_code' : error_code,
+            'puzzle_id' : puzzle_id
+        }
+
     return ret
 
 def new_specific_puzzle(request,puzzle_id):
     
     context = set_up_new_puzzle(puzzle_id)
-    template = loader.get_template('sudoku_app/index.html')
+    if (context["error_code"] == 0):
+        template = loader.get_template('sudoku_app/index.html')
+    else:
+        template = loader.get_template('sudoku_app/error_page.html')
     return HttpResponse(template.render(context, request))
 
 def new_puzzle(request):
