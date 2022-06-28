0d9da1a7540126c6b7dfd28459b7dac92cf8a6cc
Alberto Corrias <alberto@nus.edu.sg>
2022-06-28 15:01:53 +0800

    adding trainiling slash to site url instead of app urls

--- sudoku_site/sudoku_app/urls.py
+++ sudoku_site/sudoku_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'sudoku_app'
 
 urlpatterns = [
-    path('/', views.index, name='index'),
-    path('/new_puzzle', views.new_puzzle, name='new_puzzle'),
-    path('/<int:puzzle_id>', views.new_specific_puzzle, name='new_spec_puzzle'),
+    path('', views.index, name='index'),
+    path('new_puzzle', views.new_puzzle, name='new_puzzle'),
+    path('<int:puzzle_id>', views.new_specific_puzzle, name='new_spec_puzzle'),
 ]
--- sudoku_site/sudoku_site/urls.py
+++ sudoku_site/sudoku_site/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('sudoku_app', include('sudoku_app.urls')),
+    path('sudoku_app/', include('sudoku_app.urls')),
     path('admin/', admin.site.urls),
 ]
