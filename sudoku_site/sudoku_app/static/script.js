1da1f50e1ce11262a87d6ae5daed3c42407a35ae
Alberto Corrias <alberto@nus.edu.sg>
2022-07-03 12:45:24 +0800

    adding requirements file

--- sudoku_site/sudoku_app/static/script.js
+++ sudoku_site/sudoku_app/static/script.js
@@ -3,4 +3,5 @@ import Game from "./Game.js"
 
 const puzzle_hints = JSON.parse(document.getElementById('puzzle_hints').textContent);
 const puzzle_solution = JSON.parse(document.getElementById('puzzle_solution').textContent);
+
 const new_game = new Game(puzzle_hints,puzzle_solution);
