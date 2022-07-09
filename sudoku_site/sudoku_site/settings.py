5735528c23b8fd24a1126d81b1f54e5563234baf
Alberto Corrias <alberto@nus.edu.sg>
2022-07-09 17:54:16 +0800

    url change

--- sudoku_site/sudoku_site/settings.py
+++ sudoku_site/sudoku_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 #ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-ALLOWED_HOSTS = ['165.22.56.50','lisudoku.org', 'www.lisudoku.org','localhost']
+ALLOWED_HOSTS = ['165.22.56.50','lisudoku.org', 'www.lisudoku.org','localhost', '127.0.0.1']
 
 # Application definition
 
--- sudoku_site/sudoku_site/urls.py
+++ sudoku_site/sudoku_site/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('sudoku_app/', include('sudoku_app.urls')),
+    path('', include('sudoku_app.urls')),
+    #path('sudoku_app/', include('sudoku_app.urls')),
     path('admin/', admin.site.urls),
 ]
