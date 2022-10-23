e18e08df78d0aa8f3cf6b215c6ef6b9bb130647c
Alberto Corrias <alberto@nus.edu.sg>
2022-10-23 14:51:44 +0800

    Style chanegs to main page

--- sudoku_site/sudoku_app/static/Globals.js
+++ sudoku_site/sudoku_app/static/Globals.js
@@ -2,7 +2,7 @@
 var Globals = {
          GRID_SIZE: 9,
          CELL_SIZE: 8,
-         BOARD_BORDER_WIDTH: 0.9,
+         /*BOARD_BORDER_WIDTH: 0.9,*/
 
          NUMBER_COLOR_OF_HINTS: "rgb(46, 97, 71)", /*this is a very dark green*/
          NUMBER_COLOR_OF_USER_NUMBERS: "#00000", /*Black*/
@@ -16,12 +16,12 @@ var Globals = {
          HIGHLIGHTED_CELL_COLOR_SAME_NUMBER: "#c1e7e7",
          NORMAL_CELL_COLOR: "#F0FFFF",
          
-         NORMAL_CELL_BORDER_STYLE: "dashed",
+         NORMAL_CELL_BORDER_STYLE: "dotted",
          QUADRANT_CELL_BORDER_STYLE: "solid",
 
          NORMAL_CELL_BORDER_WIDTH: "0.1vmin",
-         QUADRANT_CELL_BORDER_WIDTH: "0.4vmin",
-         BOARD_BORDER_WIDTH: "0.8vmin",/*Note this is double the quadrant*/
+         QUADRANT_CELL_BORDER_WIDTH: "0.1vmin",
+         BOARD_BORDER_WIDTH: "0.3vmin",
 
          CELL_BORDER_COLOR: "#333333",
          WRONG_ANSWER_BG_COLOUR: "#e57c4e",
