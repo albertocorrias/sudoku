587db8f8fb751a2a00b1d8003ab05283bb665388
albertocorrias <alberto.corrias@gmail.com>
2022-11-05 09:42:15 +0000

    Added usage of python-dotenv for environmental variables in production. Told git to ignore the .env file

--- sudoku_site/sudoku_site/settings.py
+++ sudoku_site/sudoku_site/settings.py
@@ -10,12 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
 from pathlib import Path
 import sys
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
+
 
 #
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
